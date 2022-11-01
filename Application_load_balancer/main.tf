f80d2c80934337cd8df690d445a411f68083389f
Chaitanya-Varma-Manthena <114049378+Chaitanya-Varma-Manthena@users.noreply.github.com>
2022-11-01 11:40:57 +0530

    Update main.tf

--- Application_load_balancer/main.tf
+++ Application_load_balancer/main.tf
@@ -1,3 +1,84 @@
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "project-chaitu-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "project-chaitu1" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-chaitu-sg.id
+  ]
+  user_data = file("data.tpl")
+  
+  tags = {
+    Name ="SERVER01"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "CHAITU"
+  }
+
+  depends_on = [ aws_security_group.project-chaitu-sg ]
+}
+resource "aws_instance" "project-chaitu2" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-chaitu-sg.id
+  ]
+  user_data = file("data.tpl")
+  
+  tags = {
+    Name ="SERVER02"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "CHAITU"
+  }
+
+  depends_on = [ aws_security_group.project-chaitu-sg ]
+}
 resource "aws_lb_target_group" "my-target-group" {
   health_check {
     interval            = 10
